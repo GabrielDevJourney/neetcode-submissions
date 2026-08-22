ef666be06c6d84425dcce55d1de12cef8a8605e5
Gabriel Pereira <gabspereira178@gmail.com>
2026-08-22 07:24:15 +0100

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        let l = 0;
+        let r = heights.length - 1;
+        let area = 0;
+        while (l < r) {
+            const current = Math.min(heights[l], heights[r]) * (r - l);
+            area = Math.max(area,current)
+
+            if (heights[l] < heights[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return area
+    }
+}
