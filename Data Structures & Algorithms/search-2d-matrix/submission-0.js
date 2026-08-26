ac925fe18544b2931177845c948e69ca19916ee4
Gabriel Pereira <gabspereira178@gmail.com>
2026-08-26 21:39:03 +0100

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        const m = matrix.length
+        const n = matrix[0].length
+        let left  = 0
+        let right = matrix.length * matrix[0].length - 1
+        while (left <= right){
+            let mid = Math.floor((left + right) / 2)
+            let row = Math.floor(mid/n)
+            let col = mid%n
+
+            if(matrix[row][col] === target){
+                return true
+            }
+
+            if(matrix[row][col] > target){
+                right = mid - 1
+            }else{
+                left = mid + 1
+            }
+        }
+        return false
+    }
+}
